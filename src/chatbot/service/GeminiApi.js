c584f313094f34d957de881c4fa6fa4fbf0ad9a9
sallapu <allapusrikanth93@gmail>
2024-08-07 14:54:28 +0530

    removed build

--- src/chatbot/service/GeminiApi.js
+++ src/chatbot/service/GeminiApi.js
@@ -1,16 +1,13 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-// Retrieve API key from environment variables
 const apiKey = process.env.REACT_APP_GOOGLE_GENERATIVE_AI_KEY;
 
 if (!apiKey) {
   throw new Error('API key is missing');
 }
 
-// Initialize the GoogleGenerativeAI instance with the API key
 const genAI = new GoogleGenerativeAI(apiKey);
 
-// Get the generative model
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export const sendMessage = async (prompt) => {
